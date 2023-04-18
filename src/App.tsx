291e854d65d7f7da458f1a0a3873c9aa52587929
HryhorenkoVlada <vlada.hryhorenko@gotoinc.co>
2023-04-18 20:08:12 +0300

    feat: add a header and a scoreboard to the main App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Header, Scoreboard } from './components'
 
 import './App.css'
 
 const App = () => {
-  return <div></div>
+  return (
+    <>
+      <Header
+        name="Mineswipper"
+        feature="flag"
+        firstAction="ctrl"
+        secondAction="click"
+      />
+      <Scoreboard
+        time="000"
+        levels={['begginer', 'intermedium', 'expert']}
+        mines="010"
+        onReset={() => null}
+      />
+    </>
+  )
 }
 
 export default App
